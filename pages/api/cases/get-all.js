5912765ad7f53804879177cf489c56928c7cd98f
redouand <bnitamoublida47@gmail.com>
2021-12-12 20:24:12 +0000

    added sass setup

---
+++ pages/api/cases/get-all.js
@@ -0,0 +1,33 @@
+import connectdb from "../../../utils/connectdb";
+import Cases from "../../../models/casesModel";
+
+// connectdb();
+
+export default async (req, res) => {
+  switch (req.method) {
+    case "GET":
+      await getcases(req, res);
+      break;
+  }
+};
+
+const getcases = async (req, res) => {
+  try {
+    const rawCase = await Cases.find();
+    const readyCase = rawCase.map(
+      ({ _id, caseName, price, content, caseImage }) => {
+        return {
+          id: _id,
+          caseName,
+          price,
+          items: content,
+          caseImage,
+        };
+      }
+    );
+
+    res.send(readyCase);
+  } catch (err) {
+    return res.status(500).json({ err: err.message });
+  }
+};

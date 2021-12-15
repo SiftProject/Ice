73b6f5dfa20bcf467654f1481a8afb7c2307dcb3
SiftProject <95536611+SiftProject@users.noreply.github.com>
2021-12-15 20:39:28 +0000

    Update verify.js

--- pages/api/auth/2fa/verify.js
+++ pages/api/auth/2fa/verify.js
@@ -20,18 +20,13 @@ const verifyauth = async (req, res) => {
         const user = result.User
         const data = await twofactor.findOne({ user })
         const {base32:secret} = data
-        console.log(data)
-        console.log(token)
+    
         const verified = speakeasy.totp.verify({
-            secret, encoding: 'base32', token: token, window: 2 
+            secret, encoding: 'base32', token: token, window: 1 
         })
        
         console.log(verified)
         if(verified) {
-            const confirmed = new twofactor({
-                Secret: secret, username: user, ascii: data.ascii, hex: data.hex, base: data.base32, otpauth_url: data.otpauth_url
-            })
-            confirmed.update()
             res.json({verified: true})
         } else {
             res.json({verified: false})

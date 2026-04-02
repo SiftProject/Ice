1ae9bedd63d19fbd5cd690de838cdbbcfa4dac87
SiftProject <95536611+SiftProject@users.noreply.github.com>
2026-04-02 23:42:12 +0000

    Update mailfunction.js

--- utils/mailfunction.js
+++ utils/mailfunction.js
@@ -7,14 +7,14 @@ async function main() {
   let transporter = nodemailer.createTransport({
     service: "Gmail",
     auth: {
-      user: "testingicecasemail@gmail.com", // generated ethereal user
-      pass: "icecase1337", // generated ethereal password
+      user: "", // generated ethereal user
+      pass: "", // generated ethereal password
     },
   });
 
   let info = await transporter.sendMail({
     from: '"Fred Foo " <testingicecasemail@gmail.com>', // sender address
-    to: "analysehf@gmail.com", // list of receivers
+    to: "", // list of receivers
     subject: "Hello ✔", // Subject line
     text: "Hello world?", // plain text body
     html: "<b>Hello world?</b>", // html body
